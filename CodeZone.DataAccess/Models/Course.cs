b5bee539bfa109d880d0f97eb229823ac71379af
MahmoudAbdElNaby136 <mahmoudabdelnaby3bnby@gmail.com>
2025-03-13 16:33:20 +0200

    Updated views

--- CodeZone.DataAccess/Models/Course.cs
+++ CodeZone.DataAccess/Models/Course.cs
@@ -10,7 +10,7 @@ namespace CodeZone.DataAccess.Models
         [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
         public string Title { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [Required(ErrorMessage = "Max capacity is required")]
         [Range(1, int.MaxValue, ErrorMessage = "Max capacity must be at least 1")]
